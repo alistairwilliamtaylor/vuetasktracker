6114f9ddb36fbf48e8b15499bb9978cce0a39ed6
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2021-05-30 21:25:41 +1000

    added Button component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,22 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green" />
     </header>
 </template>
 
 <script>
-    export default {
-        name: 'Header',
-        props: {
-            title: String
-        }
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String
+    },
+    components: {
+        Button
     }
+}
 </script>
 
 <style scoped>
